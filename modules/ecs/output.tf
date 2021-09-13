0b9442e2b2d53567f7d54df8bb09da0b50e80a0a
Renan Zelaya <renan_zelaya@hotmail.com>
2021-09-13 11:32:14 -0600

    add service name output

--- modules/ecs/output.tf
+++ modules/ecs/output.tf
@@ -2,3 +2,8 @@ output "ecs_service_id" {
   description = "ECS Service Id"
   value       = aws_ecs_service.ecs_service.id
 }
+
+output "ecs_service_name" {
+  description = "ECS Service name"
+  value       = aws_ecs_service.ecs_service.name
+}
