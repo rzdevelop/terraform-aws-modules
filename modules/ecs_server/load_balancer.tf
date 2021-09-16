8d914a8f5637c5095ed1527c3fa5995489632ec0
Renan Zelaya <renanzelaya@herounit.io>
2021-09-15 23:34:20 -0600

    Remove local usage for empty state

--- modules/ecs_server/load_balancer.tf
+++ modules/ecs_server/load_balancer.tf
@@ -1,5 +1,5 @@
 module "alb" {
-  count  = var.enable_load_balancer ? local.prevent_lb_creation ? 0 : 1 : 0
+  count  = var.enable_load_balancer ? length(var.load_balancer_arn) > 0 && length(var.load_balancer_name) > 0 && length(var.target_group_arn) > 0 && length(var.lb_security_group_id) > 0 ? 0 : 1 : 0
   source = "../load_balancer"
 
   full_name   = local.full_name
@@ -13,18 +13,18 @@ module "alb" {
 }
 
 data "aws_lb" "existing" {
-  count = var.enable_load_balancer ? local.prevent_lb_creation ? 1 : 0 : 0
+  count = var.enable_load_balancer ? length(var.load_balancer_arn) > 0 && length(var.load_balancer_name) > 0 && length(var.target_group_arn) > 0 && length(var.lb_security_group_id) > 0 ? 1 : 0 : 0
   arn   = var.load_balancer_arn
   name  = var.load_balancer_name
 }
 
 data "aws_security_group" "existing" {
-  count = var.enable_load_balancer ? local.prevent_lb_creation ? 1 : 0 : 0
+  count = var.enable_load_balancer ? length(var.load_balancer_arn) > 0 && length(var.load_balancer_name) > 0 && length(var.target_group_arn) > 0 && length(var.lb_security_group_id) > 0 ? 1 : 0 : 0
   id    = var.lb_security_group_id
 }
 
 data "aws_lb_target_group" "existing" {
-  count = var.enable_load_balancer ? local.prevent_lb_creation ? 1 : 0 : 0
+  count = var.enable_load_balancer ? length(var.load_balancer_arn) > 0 && length(var.load_balancer_name) > 0 && length(var.target_group_arn) > 0 && length(var.lb_security_group_id) > 0 ? 1 : 0 : 0
   arn   = var.target_group_arn
 }
 
@@ -32,7 +32,7 @@ resource "aws_security_group_rule" "ingresses" {
   for_each = { for vm in var.enable_load_balancer ? [var.containers_data[0]] : [] : vm.port => vm }
 
   type              = "ingress"
-  security_group_id = var.enable_load_balancer ? local.prevent_lb_creation ? data.aws_security_group.existing[0].id : module.alb[0].security_group_id : null
+  security_group_id = var.enable_load_balancer ? length(var.load_balancer_arn) > 0 && length(var.load_balancer_name) > 0 && length(var.target_group_arn) > 0 && length(var.lb_security_group_id) > 0 ? data.aws_security_group.existing[0].id : module.alb[0].security_group_id : null
 
   protocol         = lookup(each.value, "protocol", "tcp")
   from_port        = each.value.port
@@ -44,7 +44,7 @@ resource "aws_security_group_rule" "ingresses" {
 # Redirect to https listener
 resource "aws_lb_listener" "http" {
   for_each          = { for vm in var.enable_load_balancer ? [var.containers_data[0]] : [] : vm.port => vm }
-  load_balancer_arn = var.enable_load_balancer ? local.prevent_lb_creation ? data.aws_lb.existing[0].arn : module.alb[0].arn : null
+  load_balancer_arn = var.enable_load_balancer ? length(var.load_balancer_arn) > 0 && length(var.load_balancer_name) > 0 && length(var.target_group_arn) > 0 && length(var.lb_security_group_id) > 0 ? data.aws_lb.existing[0].arn : module.alb[0].arn : null
   port              = each.value.port
   protocol          = "HTTP"
 
@@ -63,7 +63,7 @@ resource "aws_lb_listener" "http" {
 resource "aws_lb_listener" "https" {
   for_each = { for vm in var.enable_load_balancer ? var.containers_data : [] : vm.port => vm }
 
-  load_balancer_arn = var.enable_load_balancer ? local.prevent_lb_creation ? data.aws_lb.existing[0].arn : module.alb[0].arn : null
+  load_balancer_arn = var.enable_load_balancer ? length(var.load_balancer_arn) > 0 && length(var.load_balancer_name) > 0 && length(var.target_group_arn) > 0 && length(var.lb_security_group_id) > 0 ? data.aws_lb.existing[0].arn : module.alb[0].arn : null
   port              = each.value.port
   protocol          = "HTTPS"
 
@@ -71,7 +71,7 @@ resource "aws_lb_listener" "https" {
   certificate_arn = var.alb_cert_arn
 
   default_action {
-    target_group_arn = var.enable_load_balancer ? local.prevent_lb_creation ? data.aws_lb_target_group.existing[0].arn : module.alb[0].target_group_arn : null
+    target_group_arn = var.enable_load_balancer ? length(var.load_balancer_arn) > 0 && length(var.load_balancer_name) > 0 && length(var.target_group_arn) > 0 && length(var.lb_security_group_id) > 0 ? data.aws_lb_target_group.existing[0].arn : module.alb[0].target_group_arn : null
     type             = "forward"
   }
 }
