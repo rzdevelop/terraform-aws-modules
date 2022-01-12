0132b3bba9b0d93fce015dcc2daa9832f0ebfd6a
Renan Zelaya <renan_zelaya@hotmail.com>
2022-01-12 12:01:04 -0600

    Use naming conventions module

--- modules/db-rds/main.tf
+++ modules/db-rds/main.tf
@@ -1,20 +1,20 @@
-module "tags" {
-  source   = "../tags"
+module "naming_conventions" {
+  source   = "../naming-conventions"
   env_name = var.env_name
   app_name = var.app_name
-  role     = var.role
+  purpose  = var.purpose
 }
 
 module "security_group" {
   source            = "../security_group"
-  full_name         = module.tags.full_name
+  full_name         = module.naming_conventions.full_name
   ingress_from_port = 5432
   ingress_to_port   = 5432
   ingress_protocol  = "tcp"
   egress_from_port  = 0
   egress_to_port    = 0
   egress_protocol   = "-1"
-  tags = merge(module.tags.default_tags, {
+  tags = merge(module.naming_conventions.default_tags, {
     Module = "SecurityGroup"
   })
 }
@@ -22,11 +22,11 @@ module "security_group" {
 module "rds" {
   source     = "../rds"
   sg_id      = module.security_group.id
-  identifier = module.tags.full_name
+  identifier = module.naming_conventions.full_name
   name       = var.db_name
   username   = var.db_username
   password   = var.db_password
-  tags = merge(module.tags.default_tags, {
+  tags = merge(module.naming_conventions.default_tags, {
     Module = "RDS"
   })
 }
